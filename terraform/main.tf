ee53d7e90287dfe749196b3b71e36213bad3219f
Jacob Garcia <janikgar@gmail.com>
2019-06-17 11:48:45 -0700

    update terraform file for single instance launch

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,15 @@
+variable "region" {
+  description = "AWS EC2 Region used"
+  default = "us-west-2"
+}
+
+
 provider "aws" {
+  profile = "default"
   region = "${var.region}"
 }
 
-module "instances" {
-  source = "./instances"
+resource "aws_instance" "pihole-single" {
+  ami = "ami-0adafe63ecb0f6ed3"
+  instance_type = "t2.micro"
 }
